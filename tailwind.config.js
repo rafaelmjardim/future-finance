8fcde3a4b03fb0ca28676d29a1530087e48404e9
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-10 08:59:34 -0300

    feat(tailwind): Adicionado configurações

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,41 @@ module.exports = {
   darkMode: 'class',
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
+    extend: { 
       colors: {
-        primary: {
-          light: '#1e293b', // Cor principal para o tema light
-          dark: '#f3f4f6',  // Cor principal para o tema dark
-        },
-        background: {
-          light: '#ffffff',
-          dark: '#000000',
-        },
-      }
+        primary: '#191919',
+        secundary: '#777777',
+        accent: '#D33535',
+        greenColor: '#6e9c90',
+        grayColor: '#6e839c',
+        backgroundColor: '#FFFFFF',
+        surfaceColor: '#FFFFFF',
+        borderColor: '#DDDDDD',
+        textColor: '#191919',
+      },
     },
+    borderRadius: {
+      'none': '0',
+      'sm': '.2rem',
+      DEFAULT: '.5rem',
+      'lg': '1rem',
+      'ful': '9999px'
+    },
+    gap: {
+      '0.5': '.5rem',
+      '0.8': '.8rem',
+      '1': '1rem',
+      '2': '2rem',
+      '3': '3rem',
+      '4': '4rem',
+      '5': '5rem',
+      '6': '6rem',
+    },
+    fontSize: {
+      'sm': '1.4rem',
+      'base': '1.6rem',
+      'lg': '1.8rem'
+    }
   },
   plugins: [],
 }
