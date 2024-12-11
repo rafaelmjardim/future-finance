91283c1595ff4cbc028e94a21728f92e33ea98c1
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-11 11:35:23 -0300

    feat(dashboard): limpando testes da dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
+import { ApiService } from '../../services/api/api.service';
+import { User } from '../../services/user/user';
+import { UtilsService } from '../../services/utils/utils.service';
 import { UserService } from './../../services/user/user.service';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +11,13 @@ import { Component, inject } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   private userService = inject(UserService);
-
-  protected user = this.userService.getUserStorge();
-
+  private api = inject(ApiService);
+  private utilsService = inject(UtilsService);
+  protected user: User = this.userService.getUserStorge();
+  
+  ngOnInit(): void {
+  } 
 }
