c308039e32f1e0a9bd7dd90bae5f11da73fa831d
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-15 21:45:14 -0300

    feat(geral): ajustes gerais e novos icons

--- src/app/constants/icons.config.ts
+++ src/app/constants/icons.config.ts
@@ -1,4 +1,4 @@
-import { ionAddOutline, ionArrowBack, ionSettings, ionSwapHorizontalOutline } from "@ng-icons/ionicons";
+import { ionAddOutline, ionArrowBack, ionBarcodeOutline, ionCardOutline, ionCarOutline, ionCashOutline, ionFastFoodOutline, ionPricetagOutline, ionSettings, ionSwapHorizontalOutline } from "@ng-icons/ionicons";
 import { lucideLayoutDashboard, lucideLayoutPanelLeft, lucideLogOut } from "@ng-icons/lucide";
 
 export const iconsConfig = {
@@ -8,5 +8,11 @@ export const iconsConfig = {
     lucideLogOut, 
     lucideLayoutPanelLeft, 
     lucideLayoutDashboard,
-    ionArrowBack
+    ionArrowBack,
+    ionFastFoodOutline,
+    ionPricetagOutline,
+    ionCardOutline,
+    ionCarOutline,
+    ionBarcodeOutline,
+    ionCashOutline
 }
