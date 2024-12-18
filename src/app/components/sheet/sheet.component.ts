477ec58a5b1ace7fabcd2e6dfb009cadc2c8a310
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-18 10:17:32 -0300

    fix(sheet): ajustado animação bugada

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -1,25 +1,14 @@
-import { animate, style, transition, trigger } from '@angular/animations';
 import { DialogModule, DialogRef } from '@angular/cdk/dialog';
+import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-sheet',
   standalone: true,
-  imports: [DialogModule, NgIcon],
+  imports: [DialogModule, NgIcon, NgClass],
   templateUrl: './sheet.component.html',
   styleUrl: './sheet.component.scss',
-  animations: [
-    trigger('sheetAnimation', [
-      transition(':enter', [
-        style({ transform: 'translateX(100%)', opacity: 0 }),
-        animate('.4s ease', style({ transform: 'translateX(0)', opacity: 1 })),
-      ]),
-      transition(':leave', [
-        animate('.4s ease', style({ transform: 'translateX(100%)', opacity: 0 })),
-      ]),
-    ]),
-  ],
 })
 export class SheetComponent {
   protected dialogRef = inject(DialogRef<SheetComponent>);
