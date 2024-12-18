2cbf9257439ab7ba1a3d9e25c77b796555a6f84e
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-18 11:15:02 -0300

    feat(sheet): Implementado formulario

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -2,11 +2,12 @@ import { DialogModule, DialogRef } from '@angular/cdk/dialog';
 import { NgClass } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-sheet',
   standalone: true,
-  imports: [DialogModule, NgIcon, NgClass],
+  imports: [DialogModule, NgIcon, NgClass, ButtonComponent],
   templateUrl: './sheet.component.html',
   styleUrl: './sheet.component.scss',
 })
