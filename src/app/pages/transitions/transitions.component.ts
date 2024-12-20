8bb79178512df55ef331c6c56934b8dc840de1e5
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-20 00:07:22 -0300

    feat: Implementado charts com apexcharts

--- src/app/pages/transitions/transitions.component.ts
+++ src/app/pages/transitions/transitions.component.ts
@@ -9,11 +9,28 @@ import { pagesItems } from '../../constants/menu';
 import { Transition } from './transitions';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CardComponent } from '../../components/card/card.component';
+import { ApexAxisChartSeries, ApexChart, ApexDataLabels, ApexFill, ApexPlotOptions, ApexTitleSubtitle, ApexXAxis, ApexYAxis, NgApexchartsModule, ApexTheme, ApexStroke, ApexTooltip, ApexLegend, ApexGrid } from 'ng-apexcharts';
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  title: ApexTitleSubtitle;
+  stroke: ApexStroke,
+  colors: string[]
+  tooltip: ApexTooltip
+  legend: ApexLegend
+  grid: ApexGrid
+};
 
 @Component({
   selector: 'app-transitions',
   standalone: true,
-  imports: [PageHeaderComponent, TransitionsListComponent, CardComponent],
+  imports: [PageHeaderComponent, TransitionsListComponent, CardComponent, NgApexchartsModule],
   templateUrl: './transitions.component.html',
   styleUrl: './transitions.component.scss'
 })
@@ -29,9 +46,40 @@ export class TransitionsComponent implements OnInit {
   protected incomings: Transition[] = [];
   protected expenses: Transition[] = [];
 
+  protected chartOptions!: Partial<ChartOptions>;
+
   ngOnInit(): void {
     this.getReceitas()
     this.getDespesas();
+
+    this.chartOptions = {
+      series: [
+        {
+          name: "Receita",
+          data: [2000, 2400, 1500],
+        },
+        {
+          name: "Despesa",
+          data: [1200, 1800, 900],
+        }
+      ],
+      colors: ['#6e9c90', '#D33535'],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      xaxis: {
+        categories: ['Janeiro', 'Fevereiro', 'Março'],
+        title: {
+          text: 'Meses',
+        },
+      },
+      yaxis: {
+        title: {
+          text: 'Valores (R$)',
+        },
+      },
+    }
   }
 
   getReceitas = () => {
