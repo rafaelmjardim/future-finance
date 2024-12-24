db3ab64ef91364fb432edde4161b75a973e2cf4e
rafaelmjardim <rafaelmjardim@hotmail.com>
2024-12-24 11:24:14 -0300

    feat(button): adicionado classes e input de tipo no iconbutton

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,10 +1,11 @@
+import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [NgIcon],
+  imports: [NgIcon, NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
@@ -12,6 +13,7 @@ export class ButtonComponent {
   @Output() actionEvent = new EventEmitter<void>();
   @Input() txt!: string;
   @Input() icon!: string;
+  @Input() iconType!: 'DELETE' | 'DEFAULT';
 
   handleAction = () => {
     this.actionEvent.emit()
