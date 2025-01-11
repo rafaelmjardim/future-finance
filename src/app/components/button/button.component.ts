89cab5f2e9a0ecd2672199126521ab85bb835f5f
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-01-11 14:33:34 -0300

    feat(button): adicionado loader ao button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,11 +1,12 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [NgIcon, NgClass],
+  imports: [NgIcon, NgClass, LoaderComponent],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
@@ -14,6 +15,7 @@ export class ButtonComponent {
   @Input() txt!: string;
   @Input() icon!: string;
   @Input() iconType!: 'DELETE' | 'DEFAULT';
+  @Input() loader = false;
 
   handleAction = () => {
     this.actionEvent.emit()
