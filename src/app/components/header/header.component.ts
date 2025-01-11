8a149de0072410b38b3071ea619632a65ec299e6
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-01-11 18:28:08 -0300

    feat(header): adicionado logo sistema header mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,20 @@
+import { MediaQueryService } from './../../services/media-query/media-query.service';
 import { Component, inject } from '@angular/core';
 import { UserService } from '../../services/user/user.service';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgIcon],
+  imports: [NgIcon, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  protected mediaQueryService = inject(MediaQueryService);
   private userService = inject(UserService);
   private autService = inject(AuthService);
   private router = inject(Router);
