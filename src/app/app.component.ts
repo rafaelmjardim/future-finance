20ebbd6d364177a03db036b4c47b05f63544ae94
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-01-11 16:54:01 -0300

    feat(app): ajustado para mudar para navbarmobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { HeaderComponent } from './components/header/header.component';
 import { AuthService } from './services/auth/auth.service';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SidebarService } from './components/sidebar/sidebar.service';
+import { NavbarMobileComponent } from './components/navbar-mobile/navbar-mobile.component';
+import { MediaQueryService } from './services/media-query/media-query.service';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +17,14 @@ import { SidebarService } from './components/sidebar/sidebar.service';
     NgClass, 
     HeaderComponent,
     SidebarComponent,
+    NavbarMobileComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
   protected authService = inject(AuthService);  
-
+  protected mediaQueryService = inject(MediaQueryService);
   protected sidebarService = inject(SidebarService);
 
   ngOnInit(): void {
