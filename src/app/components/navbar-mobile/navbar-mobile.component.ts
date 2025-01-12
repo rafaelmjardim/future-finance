659a5e5af358a0d8dd03eba686d175225e049b62
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-01-12 18:12:38 -0300

    feat(navbar-mobile): adicionado selected class

--- src/app/components/navbar-mobile/navbar-mobile.component.ts
+++ src/app/components/navbar-mobile/navbar-mobile.component.ts
@@ -1,19 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { NgIcon } from '@ng-icons/core';
 import { ButtonComponent } from '../button/button.component';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Dialog } from '@angular/cdk/dialog';
 import { SheetComponent } from '../sheet/sheet.component';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar-mobile',
   standalone: true,
-  imports: [NgIcon, ButtonComponent, RouterLink],
+  imports: [NgIcon, ButtonComponent, RouterLink, NgClass],
   templateUrl: './navbar-mobile.component.html',
   styleUrl: './navbar-mobile.component.scss'
 })
 export class NavbarMobileComponent {
   private dialog = inject(Dialog);
+  protected router = inject(Router);
 
   public openSheet = () => {
     this.dialog.open(SheetComponent);
