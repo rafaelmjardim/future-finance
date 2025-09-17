a2dce807010c02fefbbfc514cd0fd0ec67341fa1
rafaelmjardim <rafaelmjardim@hotmail.com>
2025-09-16 21:43:46 -0300

    fix: header formatado e ajustado cdk menu

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -6,14 +6,14 @@ import { AuthService } from '../../../shared/services/auth/auth.service';
 import { Router } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 import { NgClass } from '@angular/common';
-import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from "@angular/cdk/menu";
+import { CdkMenu, CdkMenuItem, CdkMenuTrigger } from '@angular/cdk/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [NgIcon, NgClass, CdkMenu, CdkMenuItem, CdkMenuTrigger],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   protected mediaQueryService = inject(MediaQueryService);
@@ -22,43 +22,43 @@ export class HeaderComponent {
   private autService = inject(AuthService);
   private router = inject(Router);
 
-  protected menuList: {icon: string, txt: string, actionFunc: Function}[] = [
+  protected menuList: { icon: string; txt: string; actionFunc: Function }[] = [
     {
       icon: 'ionSparklesOutline',
       txt: 'Upgrade Premium',
-      actionFunc: () => ''
+      actionFunc: () => '',
     },
     {
       icon: 'ionSettingsOutline',
       txt: 'Config',
-      actionFunc: () => this.changeDarkMode()
+      actionFunc: () => this.changeDarkMode(),
     },
     {
       icon: 'ionPersonOutline',
       txt: 'Conta',
-      actionFunc: () => {} 
+      actionFunc: () => {},
     },
     {
       icon: 'lucideLogOut',
       txt: 'Sair',
-      actionFunc: () => this.handleLogout()
+      actionFunc: () => this.handleLogout(),
     },
-  ]
-
+  ];
 
   protected handleLogout = () => {
-    this.autService.signOut()
-    .then(logout_response => {
-      console.log('Fez logout', logout_response);
-      this.userService.removeUserStorge();
-      this.router.navigateByUrl('/login');
-    })
-    .catch(error_response => {
-      console.log('Error: ', error_response);
-    })
-  }
+    this.autService
+      .signOut()
+      .then((logout_response) => {
+        console.log('Fez logout', logout_response);
+        this.userService.removeUserStorge();
+        this.router.navigateByUrl('/login');
+      })
+      .catch((error_response) => {
+        console.log('Error: ', error_response);
+      });
+  };
 
   protected changeDarkMode = () => {
-    this.utilsService.darkModeSignal.update(value => !value);
-  }
+    this.utilsService.darkModeSignal.update((value) => !value);
+  };
 }
