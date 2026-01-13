99d86f60f2966f89a9ffcd4f79c2d17a80b250c1
rafaelmjardim <rafaelmjardim@hotmail.com>
2026-01-13 14:28:11 -0300

    feat: adiciona size dinamico loader

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
-    selector: 'app-loader',
-    imports: [],
-    templateUrl: './loader.component.html',
-    styleUrl: './loader.component.scss'
+  selector: 'app-loader',
+  imports: [],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
 })
 export class LoaderComponent {
-
+  public size = input(24);
 }
